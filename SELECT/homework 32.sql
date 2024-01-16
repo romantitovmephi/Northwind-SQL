57c8aea538d8757b91f5b7476f36961ba5826afe
Roman Titov <107221825+romantitovmephi@users.noreply.github.com>
2024-01-16 14:44:28 +0300

    Update homework 32.sql

--- SELECT/homework 32.sql
+++ SELECT/homework 32.sql
@@ -1,29 +1,32 @@
+-- 1
 SELECT *
 FROM orders
 WHERE ship_country LIKE 'U%'
 
+-- 2
 SELECT order_id, customer_id, freight, ship_country
 FROM orders
 WHERE ship_country LIKE 'N%'
 ORDER BY freight DESC
 LIMIT 10
 
+-- 3
 SELECT first_name, last_name, home_phone, region
 FROM employees
 WHERE region IS NULL 
 
+-- 4
 SELECT COUNT(customer_id)
 FROM customers
 WHERE region IS NOT NULL
 
+-- 5
 SELECT country, COUNT(*)
 FROM suppliers
 GROUP BY country 
 ORDER BY COUNT(*) DESC
 
-SELECT *
-FROM orders
-
+-- 6
 SELECT ship_country, SUM(freight)
 FROM orders
 WHERE ship_region IS NOT NULL
@@ -31,6 +34,7 @@ GROUP BY ship_country
 HAVING SUM(freight) > 2750
 ORDER BY SUM(freight) DESC
 
+-- 7
 SELECT country
 FROM customers
 UNION
@@ -38,6 +42,7 @@ SELECT country
 FROM suppliers
 ORDER BY country
 
+-- 8
 SELECT country
 FROM customers
 INTERSECT
@@ -47,6 +52,7 @@ INTERSECT
 SELECT country
 FROM employees
 
+-- 9
 SELECT country
 FROM customers
 INTERSECT
