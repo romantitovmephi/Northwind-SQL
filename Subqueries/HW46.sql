e3ad7a7b905137f90f441d902b81f5da248234fa
Roman Titov <107221825+romantitovmephi@users.noreply.github.com>
2024-01-19 22:06:09 +0300

    Update HW46.sql

--- Subqueries/HW46.sql
+++ Subqueries/HW46.sql
@@ -3,17 +3,20 @@ SELECT DISTINCT product_name, units_in_stock
 FROM products
 JOIN order_details USING(product_id)
 WHERE units_in_stock < ALL (SELECT AVG(quantity)
-					        FROM order_details
-					        GROUP BY product_id)
+			    FROM order_details
+			    GROUP BY product_id)
 ORDER BY units_in_stock DESC
 
---2 немного не согласен с автором
+--2 
 SELECT customer_id, SUM(freight) AS freight_sum
 FROM orders
-WHERE freight >= ANY(SELECT AVG(freight)
-                     FROM orders) AND shipped_date BETWEEN '1996-07-16' AND '1996-07-31'
+JOIN (SELECT customer_id, AVG(freight) AS freight_avg
+      FROM orders
+      GROUP BY customer_id) oa
+USING(customer_id)
+WHERE freight > freight_avg AND shipped_date BETWEEN '1996-07-16' AND '1996-07-31'
 GROUP BY customer_id
-ORDER BY freight_sum DESC
+ORDER BY freight_sum 
 
 --3 заджоинил вместо подзапроса
 SELECT orders.order_id, customer_id, ship_country, order_date, SUM(unit_price * quantity * (1 - discount)) AS order_price
@@ -28,8 +31,8 @@ LIMIT 3
 SELECT customer_id, ship_country, order_price
 FROM orders
 JOIN (SELECT order_id, SUM(unit_price * quantity * (1 - discount)) AS order_price
-	  FROM order_details
-	  GROUP BY order_id) AS od
+      FROM order_details
+      GROUP BY order_id) AS od
 USING(order_id)
 WHERE ship_country IN ('Argentina', 'Venezuela', 'Mexico', 'Brazil', 'Bolivia', 'Chile', 'Colombia', 'Ecuador', 'Guyana') AND order_date >= '1997-09-01'
 ORDER BY order_price DESC
