275845d4b50c411a6933ec2a22d8e4e469eb0acf
Roman Titov <107221825+romantitovmephi@users.noreply.github.com>
2024-01-27 17:34:57 +0300

    Update HW127.sql

--- Window function/HW127.sql
+++ Window function/HW127.sql
@@ -1,30 +1,13 @@
 --1
---к каждому employee_id привязаны order_id
---подзапрос считает сумму всех заказов и РАЗДЕЛЯЕТ по employee_id (PARTITION BY employee_id)
---DISTINCT employee_id - иначе он будет дублировать все заказы каждого сотрудника
---подзапрос:
-	SELECT DISTINCT employee_id, first_name, last_name, salary,
-	SUM(unit_price*quantity) OVER (PARTITION BY employee_id) AS total_sales_employee
-	FROM orders
-	LEFT JOIN order_details USING(order_id)
-	JOIN employees USING(employee_id)
-	ORDER BY total_sales_employee DESC
-
-
---сам запрос
-SELECT DISTINCT employee_id, first_name, last_name, salary, total_sales_employee, 
-AVG(total_sales_employee) OVER() AS avg_sales -- оконная функция AVG - окно тут - все строки результата запроса, нет какой-то категории (PARTITION)
-FROM (
-	SELECT DISTINCT employee_id, first_name, last_name, salary,
-	SUM(unit_price*quantity) OVER (PARTITION BY employee_id) AS total_sales_employee
-	FROM orders
-	LEFT JOIN order_details USING(order_id)
-	JOIN employees USING(employee_id)
-	ORDER BY total_sales_employee DESC
-) subquery
-ORDER BY total_sales_employee DESC
-
-
+SELECT employee_id, first_name, last_name, sales_amount,
+AVG(sales_amount) OVER() AS avg_sales_amount -- окно пустое OVER () - значит окно это все строки результата запроса
+FROM (SELECT employee_id, first_name, last_name, SUM(unit_price * quantity) AS sales_amount
+	  FROM employees
+	  JOIN orders USING(employee_id)
+	  JOIN order_details USING(order_id)
+      GROUP BY employee_id
+      ORDER BY employee_id) subquery -- делаю обычный запрос с суммой продаж каждого сотрудника
+ORDER BY sales_amount DESC
 
 --2
 SELECT first_name, last_name, title, salary,
